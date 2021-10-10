3d4494db1690771c030ad926d2e35bb8ecfe41d4
iliatapazukk <74911110+iliatapazukk@users.noreply.github.com>
2021-10-10 09:36:48 +0300

    yandex maps (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Location from './components/Location'
 import './styles/App.scss'
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <Location/>
     </div>
   );
 }
